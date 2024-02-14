991d9ac40ab345f3809fb0d0bf0aaabc0b11396a
Simone Veitch <simone.veitch@gmail.com>
2024-02-14 14:47:25 +1100

    Complete lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string .toUpperCase();
+}
+
+function whisper(string) {
+    return string .toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string .toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string .toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate (string) {
+    let message;
+    if (string === whisper(string)) {
+        message = 'I can\'t hear you!';
+    } else if (string === shout(string)) {
+        message = 'YES INDEED!';
+    } else if (string === "Let\'s have dinner together!") {
+        message = 'I would love to!';
+    } 
+    return message;
+}
